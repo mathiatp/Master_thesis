Step 1 refactorting. Restructured pixel_pos calculation. Added removal of walls with masks. Added new method to get rgbvalues for BEW. Added new check for distance with pixel_pos: This was a bug/error from project thesis

Bird_eye_view_real_time.py
@@ -11,8 +11,7 @@ import scipy
 from yaml.loader import SafeLoader
 from cls_mA2 import mA2
 from cls_Camera import Camera
-from calculate_bew_data import calculate_BEW_points_and_rgb_for_interpolation, interpolate, interp_weights
-from get_black_fill_pos_rgb import get_black_pixel_pos_and_rgb
+from calculate_bew_data import calculate_BEW_points_and_rgb_for_interpolation,calculate_rgb_matrix_for_BEW, interp_weights, interpolate
 from config import BEW_IMAGE_HEIGHT, BEW_IMAGE_WIDTH
 import cProfile
 import pstats
@@ -39,25 +38,47 @@ def individual_color_interpolation_mp(args):
 
 def make_BEW(vessel_mA2: mA2):
     start = time.time()
-    points_fp_f, rgb_fp_f = calculate_BEW_points_and_rgb_for_interpolation(vessel_mA2.fp_f.camera_rotation, vessel_mA2.fp_f.pixel_positions, vessel_mA2.fp_f.im)
-    points_fs_f, rgb_fs_f = calculate_BEW_points_and_rgb_for_interpolation(vessel_mA2.fs_f.camera_rotation, vessel_mA2.fs_f.pixel_positions, vessel_mA2.fs_f.im)
-    points_fs_s, rgb_fs_s = calculate_BEW_points_and_rgb_for_interpolation(vessel_mA2.fs_s.camera_rotation, vessel_mA2.fs_s.pixel_positions, vessel_mA2.fs_s.im)
-    points_ap_p, rgb_ap_p = calculate_BEW_points_and_rgb_for_interpolation(vessel_mA2.ap_p.camera_rotation, vessel_mA2.ap_p.pixel_positions, vessel_mA2.ap_p.im)
-    points_ap_a, rgb_ap_a = calculate_BEW_points_and_rgb_for_interpolation(vessel_mA2.ap_a.camera_rotation, vessel_mA2.ap_a.pixel_positions, vessel_mA2.ap_a.im)
-    points_as_a, rgb_as_a = calculate_BEW_points_and_rgb_for_interpolation(vessel_mA2.as_a.camera_rotation, vessel_mA2.as_a.pixel_positions, vessel_mA2.as_a.im)
-    points_as_s, rgb_as_s = calculate_BEW_points_and_rgb_for_interpolation(vessel_mA2.as_s.camera_rotation, vessel_mA2.as_s.pixel_positions, vessel_mA2.as_s.im)
+    # points_fs_f_1, rgb_fs_f_1 = calculate_BEW_points(vessel_mA2.fs_f.pixel_positions, vessel_mA2.fs_f.im)
+
+    # points_fp_f, rgb_fp_f = calculate_BEW_points_and_rgb_for_interpolation(vessel_mA2.fp_f.pixel_positions, vessel_mA2.fp_f.im)
+    # points_fs_f, rgb_fs_f = calculate_BEW_points_and_rgb_for_interpolation(vessel_mA2.fs_f.pixel_positions, vessel_mA2.fs_f.im)
+    # points_fs_s, rgb_fs_s = calculate_BEW_points_and_rgb_for_interpolation(vessel_mA2.fs_s.pixel_positions, vessel_mA2.fs_s.im)
+    # points_ap_p, rgb_ap_p = calculate_BEW_points_and_rgb_for_interpolation(vessel_mA2.ap_p.pixel_positions, vessel_mA2.ap_p.im)
+    # points_ap_a, rgb_ap_a = calculate_BEW_points_and_rgb_for_interpolation(vessel_mA2.ap_a.pixel_positions, vessel_mA2.ap_a.im)
+    # points_as_a, rgb_as_a = calculate_BEW_points_and_rgb_for_interpolation(vessel_mA2.as_a.pixel_positions, vessel_mA2.as_a.im)
+    # points_as_s, rgb_as_s = calculate_BEW_points_and_rgb_for_interpolation(vessel_mA2.as_s.pixel_positions, vessel_mA2.as_s.im)
     
+    # print(points_fs_f == points_fs_f_1)
+
+
     grid_x,grid_y = np.meshgrid(range(BEW_IMAGE_HEIGHT), range(BEW_IMAGE_WIDTH), indexing='ij')
 
-    points = np.vstack((points_fp_f,
-                        points_fs_f,
-                        points_fs_s, 
-                        points_ap_p, 
-                        points_ap_a,
-                        points_as_a,
-                        points_as_s,
+    # points = np.vstack((points_fp_f,
+    #                     points_fs_f,
+    #                     points_fs_s, 
+    #                     points_ap_p, 
+    #                     points_ap_a,
+    #                     points_as_a,
+    #                     points_as_s,
+    #                     vessel_mA2.black_pixel_pos))
+
+    points = np.vstack((vessel_mA2.fp_f.pixel_positions_masked,
+                        vessel_mA2.fs_f.pixel_positions_masked,
+                        vessel_mA2.fs_s.pixel_positions_masked,
+                        vessel_mA2.ap_p.pixel_positions_masked,
+                        vessel_mA2.ap_a.pixel_positions_masked,
+                        vessel_mA2.as_a.pixel_positions_masked,
+                        vessel_mA2.as_s.pixel_positions_masked,
                         vessel_mA2.black_pixel_pos))
     # np.save('points.npy',points)
+
+    rgb_fp_f = calculate_rgb_matrix_for_BEW(vessel_mA2.fp_f.im,vessel_mA2.fp_f.image_mask)
+    rgb_fs_f = calculate_rgb_matrix_for_BEW(vessel_mA2.fs_f.im,vessel_mA2.fs_f.image_mask) 
+    rgb_fs_s = calculate_rgb_matrix_for_BEW(vessel_mA2.fs_s.im,vessel_mA2.fs_s.image_mask) 
+    rgb_ap_p = calculate_rgb_matrix_for_BEW(vessel_mA2.ap_p.im,vessel_mA2.ap_p.image_mask)
+    rgb_ap_a = calculate_rgb_matrix_for_BEW(vessel_mA2.ap_a.im,vessel_mA2.ap_a.image_mask)
+    rgb_as_a = calculate_rgb_matrix_for_BEW(vessel_mA2.as_a.im,vessel_mA2.as_a.image_mask)
+    rgb_as_s = calculate_rgb_matrix_for_BEW(vessel_mA2.as_s.im,vessel_mA2.as_s.image_mask)
 
     rgb = np.vstack((rgb_fp_f,
                      rgb_fs_f, 
@@ -94,13 +115,13 @@ def make_BEW(vessel_mA2: mA2):
     # return im
 
     # Delaunay 4
-    # xy = points
-    # uv=np.zeros([grid_x.shape[0]*grid_y.shape[1],2])
-    # uv[:,0]=grid_y.flatten()
-    # uv[:,1]=grid_x.flatten()
+    xy = points
+    uv=np.zeros([grid_x.shape[0]*grid_y.shape[1],2])
+    uv[:,0]=grid_y.flatten()
+    uv[:,1]=grid_x.flatten()
     values = rgb
 
-    # Computed once and for all !
+    # # Computed once and for all !
     # vtx, wts = interp_weights(xy, uv)
     # np.save('vtx.npy', vtx)
     # np.save('wts.npy', wts)
@@ -111,8 +132,8 @@ def make_BEW(vessel_mA2: mA2):
     valuesi_r=interpolate(values[:,0].flatten(), vtx, wts)
     valuesi_g=interpolate(values[:,1].flatten(), vtx, wts)
     valuesi_b=interpolate(values[:,2].flatten(), vtx, wts)
-    # end = time.time()
-    # print('Time: ' + str(end-start))
+    end = time.time()
+    print('Time: ' + str(end-start))
 
     valuesi_r = valuesi_r.reshape((grid_x.shape[0],grid_x.shape[1]),order='F')
     valuesi_r = valuesi_r.astype(np.uint8)
@@ -120,8 +141,6 @@ def make_BEW(vessel_mA2: mA2):
     valuesi_g= valuesi_g.reshape((grid_x.shape[0],grid_x.shape[1]),order='F')
     valuesi_g = valuesi_g.astype(np.uint8)
     
-    
-
     valuesi_b= valuesi_b.reshape((grid_x.shape[0],grid_x.shape[1]),order='F')
     valuesi_b = valuesi_b.astype(np.uint8)
  
@@ -132,7 +151,7 @@ def make_BEW(vessel_mA2: mA2):
 
 
     # Griddata
-    # grid_z0 = griddata(points, rgb[:], (grid_x, grid_y), method='nearest')
+    # grid_z0 = griddata(points, rgb, (grid_x, grid_y), method='nearest')
     # grid_z0[np.where(np.isnan(grid_z0))] = 0
     # grid_z0 = grid_z0[:,:,:].astype(np.uint8)
     # end = time.time()
@@ -202,6 +221,7 @@ def main():
                 
             if(id =='rgb_cam_fp_f'):
                 vessel_mA2.fp_f._im = cv2.undistort(img,vessel_mA2.fp_f.K,vessel_mA2.fp_f.D)
+                
      
             elif(id =='rgb_cam_fs_f'):
                 vessel_mA2.fs_f._im = cv2.undistort(img,vessel_mA2.fs_f.K,vessel_mA2.fs_f.D)
@@ -230,7 +250,7 @@ def main():
 
             # plt.figure('Img_distorted')
             # plt.imshow(img)
-                # plt.imsave('First 360 view_7_cameras.png', img_bew)
+                # plt.imsave('BEW_main_func_1_frame_1500x1500px_step_1_restructure.png', img_bew)
                 # plt.figure('Img_undistorted_as_a')
                 # plt.imshow(vessel_mA2.as_a.im)
                 # plt.figure('BEW')
@@ -250,4 +270,4 @@ if __name__ == '__main__':
     with cProfile.Profile() as pr:
         main()
     stats = pstats.Stats(pr)
-    # stats.dump_stats(filename='./Profiler_stats/Ros2/main_func_10_frame_1500x1500px_nearest_pre_calc_we.prof')
+    # stats.dump_stats(filename='./Profiler_stats/Ros2/main_func_10_frame_1500x1500px_step_1_restructur.prof')

calculate_bew_data.py
@@ -1,8 +1,10 @@
 import numpy as np
 import cv2
-from config import MAX_DIST_X, MIN_DIST_X, MAX_DIST_Y, MIN_DIST_Y, BEW_IMAGE_HEIGHT, BEW_IMAGE_WIDTH
+from config import MAX_DIST_X, MIN_DIST_X, MAX_DIST_Y, MIN_DIST_Y, BEW_IMAGE_HEIGHT, BEW_IMAGE_WIDTH, ROW_CUTOFF
 from scipy.interpolate import griddata
 from scipy.spatial import Delaunay
+from get_black_fill_pos_rgb import im_mask_walls
+import matplotlib.pyplot as plt
 
 def georeference_point_eq(intrinsic_matrix: np.ndarray,
                             image_points: np.ndarray,
@@ -82,7 +84,7 @@ def calculate_im_pos(height, width, K, camera_rotation, camera_translation, name
         np.save(file_name_pixel_position, im_pos)     
         return im_pos
 
-def normalize_im_pos_for_BEW(camera_rotation, im_pos_cut_t):
+def normalize_im_pos_for_BEW(im_pos_cut_t):
     im_pos_cut_F = im_pos_cut_t
     # Swithing from x forward, y right axis to x right, y forward to match image cooridnates
     switch_xy = np.array([[0,1,0],
@@ -112,15 +114,16 @@ def normalize_im_pos_for_BEW(camera_rotation, im_pos_cut_t):
 
 
 
-def calculate_BEW_points_and_rgb_for_interpolation(camera_rotation, img_pos, img_undistorted):
-    row_cut_off = 526
+def calculate_BEW_points_and_rgb_for_interpolation(img_pos, img_undistorted):
+    row_cut_off = ROW_CUTOFF
     rgb_chan = 3
     im_pos_cut = img_pos[row_cut_off:,:,:]
     im_pos_cut_T = np.einsum('ijk->kij',im_pos_cut)
     
+    
     im_cut = img_undistorted[row_cut_off:,:,:]
-    im_pos_normalized = normalize_im_pos_for_BEW(camera_rotation,im_pos_cut_T)
-
+    im_pos_normalized = normalize_im_pos_for_BEW(im_pos_cut_T)
+    
 
     new_h, new_w = 1024, 1224
 
@@ -129,14 +132,16 @@ def calculate_BEW_points_and_rgb_for_interpolation(camera_rotation, img_pos, img
                     [0,           0,          1]])
 
     im_pos_pixel = np.einsum('ij,jkl->ikl', K, im_pos_normalized)
+    
     im_pos_pixel = np.einsum('ijk->jki',im_pos_pixel)
-
+    
+    # any_nan = np.isnan(im_pos_pixel).any()
     im_pos_pixel =  np.nan_to_num(im_pos_pixel, nan = 99999999)
     im_pos_pixel = im_pos_pixel.astype(int)
-    im_pos_pixel = im_pos_pixel[:,:,:2]
+    im_pos_pixel = im_pos_pixel[:,:,:2] #(498, 1224, 2)
 
-    points_x_all = im_pos_pixel[:,:,1]
-    points_x = np.transpose(np.array([np.ravel(points_x_all)]))
+    points_x_all = im_pos_pixel[:,:,1] #(498, 1224)
+    points_x = np.transpose(np.array([np.ravel(points_x_all)])) #(609552, 1)
     points_x_all = np.transpose(np.array([np.ravel(points_x_all)]))
     points_x = np.transpose(np.array([points_x[points_x != 99999999]]))
 
@@ -158,3 +163,119 @@ def calculate_BEW_points_and_rgb_for_interpolation(camera_rotation, img_pos, img
     # grid_z0 = grid_z0[:,:,:].astype(np.uint8)
     
     return  points, rgb 
+
+def calculate_BEW_points_and_mask(img_pos: np.array, wall_mask: np.array):
+    
+    row_cut_off = ROW_CUTOFF
+    rgb_chan = 3
+    # img_pos 1024,1224,3
+    im_pos_cut = img_pos[row_cut_off:,:,:]  # 498,1224,3
+    im_pos_cut_T = np.einsum('ijk->kij',im_pos_cut) # 3,498,1224
+    
+    wall_mask = wall_mask
+    wall_mask = wall_mask[row_cut_off:,:] #498,1224, type=bool
+    image_mask = np.array([wall_mask]) # 1, 498,1224
+    image_mask[0,:,:][np.where(im_pos_cut_T[0,:,:]> MAX_DIST_X)] = False
+    image_mask[0,:,:][np.where(im_pos_cut_T[0,:,:]< MIN_DIST_X)] = False
+    image_mask[0,:,:][np.where(im_pos_cut_T[1,:,:]< MIN_DIST_Y)] = False
+    image_mask[0,:,:][np.where(im_pos_cut_T[1,:,:]> MAX_DIST_Y)] = False # 1, 498,1224
+
+
+    max_x = MAX_DIST_X
+    min_x = MIN_DIST_X
+    
+    max_y = MAX_DIST_Y
+    min_y = MIN_DIST_Y
+
+    s_x = 2/(max_x-min_x)
+    t_x = -s_x * min_x-1
+
+    s_y = 2/(max_y-min_y)
+    t_y = -s_y * min_y-1
+
+    Normalizing = np.array([[s_x, 0,  t_x],
+                            [0, s_y,  t_y],
+                            [0, 0,    1]])
+
+    switch_xy = np.array([[0,1,0],
+                            [1,0,0],
+                            [0,0,1]])
+    N = switch_xy@Normalizing
+
+    im_pos_cut_T_normalized = np.einsum('ij,jkl->ikl', N, im_pos_cut_T) # 3,498,1224
+    
+    # check_im_pos_normalized = im_pos_cut_T_normalized
+    # check_im_pos_normalized = np.einsum('ijk->jki',check_im_pos_normalized)
+    # check_im_pos_normalized = check_im_pos_normalized[:,:,:2]
+   
+    # check_im_pos_normalized[:,:,:][np.where(image_mask[0,:,:] == False)] = 0
+    # x_max = np.max(check_im_pos_normalized[:,:,0])
+    # y_max = np.max(check_im_pos_normalized[:,:,1])
+
+    # x_min = np.min(check_im_pos_normalized[:,:,0])
+    # y_min = np.min(check_im_pos_normalized[:,:,1])
+    
+
+    new_h, new_w = BEW_IMAGE_HEIGHT, BEW_IMAGE_WIDTH
+
+    K = np.array([[new_w/2-1,     0,          new_w/2],
+                    [0,           -(new_h/2-1),    new_h/2],
+                    [0,           0,          1]])
+
+    im_pos_pixel = np.einsum('ij,jkl->ikl', K, im_pos_cut_T_normalized) # 3,498,1224
+    #same here
+
+    
+    
+    # Test to get the same pixel_pos matrix to match rgb matrix - This is from old method
+    # im_pos_pixel_1 = im_pos_pixel
+    # im_pos_pixel_1 = np.einsum('ijk->jki',im_pos_pixel_1)
+    # im_pos_pixel_1 = im_pos_pixel_1[:,:,:2] #(498, 1224, 2)
+    # points_x = im_pos_pixel_1[:,:,1] #(498, 1224)
+    # points_x = np.transpose(np.array([np.ravel(points_x)])) #(609552, 1)
+    # points_y = im_pos_pixel_1[:,:,0] #(498, 1224)
+    # points_y = np.transpose(np.array([np.ravel(points_y)])) #(609552, 1)
+    # points = np.concatenate((points_x,points_y), axis=1) #(609552, 2)
+
+    im_pos_pixel = np.einsum('ij,jkl->ikl', switch_xy, im_pos_pixel) # 3,498,1224
+    im_pos_pixel = im_pos_pixel[:2,:,:] # 2,498,1224
+    im_pos_pixel = np.einsum('ijk->jki',im_pos_pixel)
+    im_pos_pixel_2_col = np.reshape(im_pos_pixel,(np.shape(im_pos_pixel)[0]*np.shape(im_pos_pixel)[1], 2)) # 609552, 2
+
+    image_mask_1_col = np.reshape(image_mask,(np.shape(im_pos_pixel_2_col)[0],1)) #(609552, 1)
+    image_mask_2_col_stack = np.column_stack((image_mask_1_col,image_mask_1_col)) # (609552, 2)
+    image_mask_3_col_stack = np.column_stack((image_mask_1_col,image_mask_1_col,image_mask_1_col)) # (609552, 3)
+
+    # im_pos_pixel = np.einsum('ijk->jk',im_pos_pixel)
+    # image_mask = np.einsum('ijk->jk', image_mask)
+    
+    # true_count = np.count_nonzero(image_mask_2_col_stack[:,0]==True)
+
+    im_pos_true = im_pos_pixel_2_col[np.all(image_mask_2_col_stack==True, axis=1)] #(499350, 2)
+    
+    
+    # im_rgb = img_undistorted[row_cut_off:,:] #(498, 1224, 3)
+    # im_rgb_T = np.reshape(im_rgb,(np.shape(im_pos_pixel_2_col)[0], 3)) #(609552, 3)
+    # im_rgb_true = im_rgb_T[np.all(image_mask_3_col_stack==True, axis=1)] # (499350, 3)
+
+
+
+    # im = img_undistorted[row_cut_off:,:,:]
+    # image_mask = np.einsum('ijk->jk', image_mask)
+    # im[:,:,:][np.where(image_mask==False)] = np.array([0,0,0])
+
+    # im = np.delete(im,np.where(image_mask==False),axis=0)
+    # im = np.reshape(im,(np.shape(im_pos_pixel)[0], 3))
+    # print(im==im_rgb_true)
+    # plt.figure()
+    # plt.imshow(im)
+    # plt.show()
+    return image_mask_3_col_stack, im_pos_true.astype(int)
+    
+
+def calculate_rgb_matrix_for_BEW(img_undistorted: np.array, image_mask: np.array):
+    im_rgb = img_undistorted[ROW_CUTOFF:,:] #(498, 1224, 3)
+    im_rgb_T = np.reshape(im_rgb,(np.shape(image_mask)[0], 3)) #(609552, 3)
+    im_rgb_true = im_rgb_T[np.all(image_mask==True, axis=1)] # (499350, 3)
+    
+    return im_rgb_true

cls_Camera.py
@@ -1,7 +1,8 @@
 import numpy as np
 from config import BEW_IMAGE_HEIGHT, BEW_IMAGE_WIDTH
 from file_handling import mA2_frame_yaml_to_rotation_translation_vec, camera_name_calib_yaml_to_K_D
-from calculate_bew_data import calculate_im_pos
+from calculate_bew_data import calculate_BEW_points_and_mask, calculate_im_pos
+from get_black_fill_pos_rgb import im_mask_walls
 
 
 
@@ -27,17 +28,9 @@ class Camera:
                                            self.camera_rotation, 
                                            self.camera_translation, 
                                            self.name)
+        self._wall_mask = im_mask_walls(name)
+        self._image_mask, self._pixel_positions_masked = calculate_BEW_points_and_mask(self.pixel_positions, self.wall_mask)
         
-
-    def update_camera_roation_and_translation(self, dict):
-        camera_key = 'eo_'+ self.name.lower()
-
-        rotation = np.array(dict[camera_key]['static_transform']['rotation'])
-        translation = np.array([dict[camera_key]['static_transform']['translation']])
-
-        self._camera_rotation = rotation
-        self._camera_translation = translation
-
 
     @property
     def name(self):
@@ -63,3 +56,13 @@ class Camera:
     @property
     def pixel_positions(self):
         return self._pixel_positions    
+    @property
+    def wall_mask(self):
+        return self._wall_mask
+    @property
+    def image_mask(self):
+        return self._image_mask
+    @property
+    def pixel_positions_masked(self):
+        return self._pixel_positions_masked 
+

cls_mA2.py
@@ -1,4 +1,4 @@
-from calculate_bew_data import calculate_BEW_points_and_rgb_for_interpolation
+from calculate_bew_data import calculate_BEW_points_and_mask, calculate_BEW_points_and_rgb_for_interpolation
 from cls_Camera import Camera
 from get_black_fill_pos_rgb import get_black_pixel_pos_and_rgb
 import numpy as np
@@ -27,20 +27,20 @@ class mA2:
         # self._delaunay = self.init_delaunay()
 
     def init_delaunay(self):
-        points_fp_f, _ = calculate_BEW_points_and_rgb_for_interpolation(self.fp_f.camera_rotation, self.fp_f.pixel_positions, cv2.imread('Images/Delaunay_init_images/delaunay_init_im_fp_f.png'))
-        points_fs_f, _ = calculate_BEW_points_and_rgb_for_interpolation(self.fs_f.camera_rotation, self.fs_f.pixel_positions, cv2.imread('Images/Delaunay_init_images/delaunay_init_im_fs_f.png'))
-        points_fs_s, _ = calculate_BEW_points_and_rgb_for_interpolation(self.fs_s.camera_rotation, self.fs_s.pixel_positions, cv2.imread('Images/Delaunay_init_images/delaunay_init_im_fs_s.png'))
-        points_ap_p, _ = calculate_BEW_points_and_rgb_for_interpolation(self.ap_p.camera_rotation, self.ap_p.pixel_positions, cv2.imread('Images/Delaunay_init_images/delaunay_init_im_ap_p.png'))
-        points_ap_a, _ = calculate_BEW_points_and_rgb_for_interpolation(self.ap_a.camera_rotation, self.ap_a.pixel_positions, cv2.imread('Images/Delaunay_init_images/delaunay_init_im_ap_a.png'))
-        points_as_a, _ = calculate_BEW_points_and_rgb_for_interpolation(self.as_a.camera_rotation, self.as_a.pixel_positions, cv2.imread('Images/Delaunay_init_images/delaunay_init_im_as_a.png'))
-        points_as_s, _ = calculate_BEW_points_and_rgb_for_interpolation(self.as_s.camera_rotation, self.as_s.pixel_positions, cv2.imread('Images/Delaunay_init_images/delaunay_init_im_as_s.png'))
-        points = np.vstack((points_fp_f,
-                            points_fs_f,
-                            points_fs_s, 
-                            points_ap_p, 
-                            points_ap_a,
-                            points_as_a,
-                            points_as_s,
+        # points_fp_f, _ = calculate_BEW_points_and_mask(self.fp_f.wall_mask, cv2.imread('Images/Delaunay_init_images/delaunay_init_im_fp_f.png'))
+        # points_fs_f, _ = calculate_BEW_points_and_mask(self.fs_f.wall_mask, cv2.imread('Images/Delaunay_init_images/delaunay_init_im_fs_f.png'))
+        # points_fs_s, _ = calculate_BEW_points_and_mask(self.fs_s.wall_mask, cv2.imread('Images/Delaunay_init_images/delaunay_init_im_fs_s.png'))
+        # points_ap_p, _ = calculate_BEW_points_and_mask(self.ap_p.wall_mask, cv2.imread('Images/Delaunay_init_images/delaunay_init_im_ap_p.png'))
+        # points_ap_a, _ = calculate_BEW_points_and_mask(self.ap_a.wall_mask, cv2.imread('Images/Delaunay_init_images/delaunay_init_im_ap_a.png'))
+        # points_as_a, _ = calculate_BEW_points_and_mask(self.as_a.wall_mask, cv2.imread('Images/Delaunay_init_images/delaunay_init_im_as_a.png'))
+        # points_as_s, _ = calculate_BEW_points_and_mask(self.as_s.wall_mask, cv2.imread('Images/Delaunay_init_images/delaunay_init_im_as_s.png'))
+        points = np.vstack((self.fp_f.pixel_positions_masked,
+                            self.fs_f.pixel_positions_masked,
+                            self.fs_s.pixel_positions_masked,
+                            self.ap_p.pixel_positions_masked,
+                            self.ap_a.pixel_positions_masked,
+                            self.as_a.pixel_positions_masked,
+                            self.as_s.pixel_positions_masked,
                             self.black_pixel_pos))
         
         return Delaunay(points)